63e17bded6d97532ba9a3bf67c3d08510f2344c1
v.alyamkin <ufna@ufna.ru>
2016-12-09 15:06:45 +0300

    Add crashlytics ios sdk

--- Source/CrashlyticsKit/CrashlyticsKit.Build.cs
+++ Source/CrashlyticsKit/CrashlyticsKit.Build.cs
@@ -4,24 +4,48 @@ using System.IO;
 
 namespace UnrealBuildTool.Rules
 {
-	public class CrashlyticsKit : ModuleRules
-	{
-		public CrashlyticsKit(TargetInfo Target)
-		{
-			PrivateIncludePaths.AddRange(
-				new string[] {
-					"CrashlyticsKit/Private",
-					// ... add other private include paths required here ...
-				});
+    public class CrashlyticsKit : ModuleRules
+    {
+        public CrashlyticsKit(TargetInfo Target)
+        {
+            const bool bEnableCrashlyticsKit = false;
+            Definitions.Add("WITH_CRASHLYTICS=" + (bEnableCrashlyticsKit ? "1" : "0"));
 
-			PublicDependencyModuleNames.AddRange(
-				new string[]
-				{
-					"Core",
-					"CoreUObject",
-					"Engine"
-					// ... add other public dependencies that you statically link with here ...
-				});
-		}
-	}
+            PrivateIncludePaths.AddRange(
+                new string[] {
+                    "CrashlyticsKit/Private",
+                    // ... add other private include paths required here ...
+                });
+
+            PublicDependencyModuleNames.AddRange(
+                new string[]
+                {
+                    "Core",
+                    "CoreUObject",
+                    "Engine"
+                    // ... add other public dependencies that you statically link with here ...
+                });
+
+            if (bEnableCrashlyticsKit)
+            {
+                // Fabric platform libraries
+                if (Target.Platform == UnrealTargetPlatform.IOS)
+                {
+                    PublicAdditionalFrameworks.Add(
+                        new UEBuildFramework(
+                            "Fabric",
+                            "../../ThirdParty/iOS/Fabric.embeddedframework.zip"
+                        )
+                    );
+
+                    PublicAdditionalFrameworks.Add(
+                        new UEBuildFramework(
+                            "Crashlytics",
+                            "../../ThirdParty/iOS/Crashlytics.embeddedframework.zip"
+                        )
+                    );
+                }
+            }
+        }
+    }
 }
