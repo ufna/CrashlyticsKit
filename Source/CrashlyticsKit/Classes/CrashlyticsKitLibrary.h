b2b83845e190383da35313a6badedc1401a42671
v.alyamkin <ufna@ufna.ru>
2017-10-24 11:59:22 +0300

    Extend logs

--- Source/CrashlyticsKit/Classes/CrashlyticsKitLibrary.h
+++ Source/CrashlyticsKit/Classes/CrashlyticsKitLibrary.h
@@ -5,6 +5,18 @@
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "CrashlyticsKitLibrary.generated.h"
 
+#if WITH_CRASHLYTICS && PLATFORM_IOS
+#import <Fabric/Fabric.h>
+#import <Crashlytics/Crashlytics.h>
+#endif // WITH_CRASHLYTICS && PLATFORM_IOS
+
+#if WITH_CRASHLYTICS && PLATFORM_IOS
+	#define CLSLOG(Text) if(UCrashlyticsKitLibrary::GetCrashlyticsProxy()->IsInitialized()) CLS_LOG(@"%@", FString(Text).GetNSString());
+#else
+	#define CLSLOG(Text) if(UCrashlyticsKitLibrary::GetCrashlyticsProxy()->IsInitialized()) UCrashlyticsKitLibrary::GetCrashlyticsProxy()->WriteLog(Text);
+#endif
+
+#define CLSERROR(Text, Code) if(UCrashlyticsKitLibrary::GetCrashlyticsProxy()->IsInitialized()) UCrashlyticsKitLibrary::GetCrashlyticsProxy()->WriteError(Text, Code);
 
 UCLASS()
 class CRASHLYTICSKIT_API UCrashlyticsKitLibrary : public UBlueprintFunctionLibrary
